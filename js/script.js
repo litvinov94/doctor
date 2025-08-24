67d7425ff27cfb92abcd06beffe87f9c404545d1
litvinov94 <vaceslavkolomejcev94@gmail.com>
2025-08-24 23:29:55 +0300

    Проект заверстан, остались правки по всему проекту

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper('.swiper', {
+    // Настройки Swiper
+    loop: true, // Бесконечная прокрутка
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true, // Делает пагинацию кликабельной
+    }
+});
