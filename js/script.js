e778b9382d8b7811182d7988417da0ed70f8b77f
litvinov94 <vaceslavkolomejcev94@gmail.com>
2025-08-25 14:24:03 +0300

    Добавлен papup, так же добавлено открытие popup в блоке header. Исправлен баг кнопок в блоке hero

--- js/script.js
+++ js/script.js
@@ -6,3 +6,19 @@ const swiper = new Swiper('.swiper', {
         clickable: true, // Делает пагинацию кликабельной
     }
 });
+
+// Popup
+
+const header__popup = document.querySelector('.header__popup');
+const popup__body = document.querySelector('.popup__body');
+
+const openPopupButtons = document.querySelector('.open-popup')
+const closePopupButton = document.querySelector('.close-popup')
+
+openPopupButtons.forEach((button) => { // Перебираем все кнопки
+    button.addEventListener('click', (e) => { // Для каждой вешаем обработчик событий на клик
+        e.preventDefault(); // Предотвращаем дефолтное поведение браузера
+        popupBg.classList.add('active'); // Добавляем класс 'active' для фона
+        popup.classList.add('active'); // И для самого окна
+    })
+});
